08157671f137488d83d860203733dfee2814fb90
Dmitriy Stakheev <dmitrii.stakhieiev@mail.ru>
2021-09-06 23:38:14 +0300

    Firts changes

--- src/main/java/lab1/Fly.java
+++ src/main/java/lab1/Fly.java
@@ -1,9 +1,8 @@
 package lab1;
 
 public class Fly implements Move {
-
     @Override
     public void move(Point a, Point b) {
-        System.out.println("I believe I can fly");
+        System.out.println("Hero is flying");
     }
 }
---
+++ src/main/java/lab1/RideHorse.java
@@ -0,0 +1,8 @@
+package lab1;
+
+public class RideHorse implements Move {
+    @Override
+    public void move(Point a, Point b) {
+        System.out.println("Hero is riding a horse");
+    }
+}
