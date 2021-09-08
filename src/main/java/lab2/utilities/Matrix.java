7b80941f05a6d6b11e1df322e245966bb77e38a7
Daniil Belichenko <mr.dancheak@gmail.com>
2021-09-08 12:39:03 +0300

    Mistake in divideByNeighbours() method in class Matrix fixed

--- src/main/java/lab2/utilities/Matrix.java
+++ src/main/java/lab2/utilities/Matrix.java
@@ -47,8 +47,8 @@ public class Matrix {
 
         int neighboursSum = 0;
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix.length; j++){
 
                 if (j + 1 <= matrix.length - 1){
                     neighboursSum += matrix[i][j + 1];
