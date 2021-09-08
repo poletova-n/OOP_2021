6ba771159119673c1af05cea5de41dace36d92bc
Daniil Belichenko <mr.dancheak@gmail.com>
2021-09-08 12:51:45 +0300

    Another small bug noticed and fixed

--- src/main/java/lab2/utilities/Matrix.java
+++ src/main/java/lab2/utilities/Matrix.java
@@ -67,16 +67,14 @@ public class Matrix {
                 }
 
                 try {
-
                     if (neighboursSum == 0){
                         throw new DividedByZero("Dividing by zero happened!");
                     }
-
-                    matrix[i][j] =Math.round((matrix[i][j] / neighboursSum) * 100.0) / 100.0;
-
                 }catch (DividedByZero exception){
                     exception.printStackTrace();
                 }
+
+                matrix[i][j] = Math.round((matrix[i][j] / neighboursSum) * 100.0) / 100.0;
 
                 neighboursSum = 0;
 
