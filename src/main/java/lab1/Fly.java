13757879872e1c9ce017fafccc47153c7a2ad78b
Эрен Ярдымджы <90216141+ErenYrdmc@users.noreply.github.com>
2021-09-12 15:09:51 +0300

    output text editing

--- src/main/java/lab1/Fly.java
+++ src/main/java/lab1/Fly.java
@@ -5,7 +5,7 @@ public class Fly implements MoveStrategy {
     @Override
     public void move(Point a, Point b) {
         if (Point.isEqual(a, b)) {
-            System.out.println("Hero is already in the right place!");
+            System.out.println("I believe I can fly");
         } else {
             System.out.println("Hero is flying from point ( " + a.getX()
                     + ", " + a.getY() + " ) to point ( " + b.getX() + ", " + b.getY() + " ).");
