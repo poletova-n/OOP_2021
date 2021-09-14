c9cc27f49faff05635e67ac155c72d0547ff3747
Евгений Смирнов <jekasjeny1012@gmail.com>
2021-09-14 11:30:00 +0300

    lab 4 git commit

---
+++ src/main/java/lab4/Employee.java
@@ -0,0 +1,60 @@
+package lab4;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface HowMuchHowMany{
+    byte value() default 0;
+}
+
+public class Employee {
+    private static int counterId = 0;
+
+    private final int id;
+    private final String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+        id = ++counterId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void   setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @HowMuchHowMany(100)
+    private void sayHi(){
+        System.out.println("Hi, I am " + name + ".");
+    }
+    @HowMuchHowMany(100)
+    private double getSalary(int hours) {
+        return salary * hours;
+    }
+    @HowMuchHowMany(5)
+    private String toJsonFormat() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("{\n");
+        sb.append("\t \"id\": \"").append(id).append("\",\n");
+        sb.append("\t \"name\": \"").append(name).append("\",\n");
+        sb.append("\t \"salary\": \"").append(salary).append("\",\n");
+        sb.append("}");
+        return  sb.toString();
+    }
+    @HowMuchHowMany(1)
+    private void work(String[] schedule) {
+        sayHi();
+        int counter = 0;
+        for (String task: schedule){
+            counter++;
+            System.out.println("Employee's id: " + id + ". Did " + "task "+ counter+ " " + "\""+task+"\".");
+        }
+        System.out.println("Good night!");
+    }
+}
--- src/main/java/lab4/Main.java
+++ src/main/java/lab4/Main.java
@@ -1,6 +1,37 @@
 package lab4;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Employee employee = new Employee("John", 10_000);
+        callDeclaredMethods(employee);
+    }
+
+    public static void callDeclaredMethods(Employee e) throws InvocationTargetException, IllegalAccessException {
+        for (Method m : e.getClass().getDeclaredMethods()) {
+            if (m.isAnnotationPresent(HowMuchHowMany.class)) {
+
+                m.setAccessible(true);
+                int number = m.getAnnotation(HowMuchHowMany.class).value();
+
+                for (int i = 0; i < number; i++) {
+                    if (m.getReturnType().equals(Void.TYPE)) {
+                        if (m.getParameterCount() < 1) {
+                            m.invoke(e);
+                        } else {
+                            m.invoke(e, (Object) new String[]{"make dinner", "to wash the dishes", "wash the floor", "to steel eggs"});
+                        }
+                    } else if (m.getReturnType().equals(Double.TYPE)) {
+                        System.out.println(m.invoke(e, 10));
+                    } else {
+                        System.out.println(m.invoke(e));
+                    }
+                }
+                System.out.println("----------------");
+            }
+        }
     }
 }
