6d1b3420eddd192ec70ea596cda09c48f98d0343
Евгений Смирнов <jekasjeny1012@gmail.com>
2021-09-15 19:31:23 +0300

    repaid problems in streams

--- src/main/java/lab5/Instruments.java
+++ src/main/java/lab5/Instruments.java
@@ -1,68 +1,92 @@
 package lab5;
 
+import javax.swing.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Instruments {
 
-    public static double getAverageOfIntegers(List<Integer> numbers){
-    double average;
-    if (numbers == null || numbers.isEmpty())
-      return 0;
-    average = numbers.stream()
-            .reduce((a, b) -> a + b)
-            .get();
-    average /= numbers.size();
-    return average;
-  }
-  public static void makeUpperCaseAndAddPrefix(List<String> strings){
-      StringBuilder sb = new StringBuilder();
-      List<String> result =
-      strings.stream()
-              .map(i -> {
-                  sb.delete(0,sb.length());
-                  sb.append("_new_").append(i.toUpperCase());
-                  return sb.toString();
-              })
-              .collect(Collectors.toList());
-      strings.removeAll(strings);
-      strings.addAll(result);
-  }
-  public static List<Double> getSingularQuadratures(List<Double> numbers){
-     return numbers.stream()
-              .distinct()
-              .map(i -> i * i)
-              .collect(Collectors.toList());
-  }
+    public static double getAverageOfIntegers(List<Integer> numbers) {
+        double average;
+        if (numbers == null || numbers.isEmpty())
+            return 0;
+        average = numbers.stream()
+                .reduce((a, b) -> a + b)
+                .get();
+        average /= numbers.size();
+        return average;
+    }
 
-  public static List<String> getListStartingWithLetterAndSort(Character letter, Collection<String> blank) {
+    public static void makeUpperCaseAndAddPrefix(List<String> strings) {
+        if (strings == null || strings.isEmpty())
+            return;
+        StringBuilder sb = new StringBuilder();
+        List<String> result =
+                strings.stream()
+                        .filter(s -> !Objects.equals(s, "") && s.length()>0)
+                        .map(i -> {
+                            sb.delete(0, sb.length());
+                            sb.append("_new_").append(i.toUpperCase());
+                            return sb.toString();
+                        })
+                        .collect(Collectors.toList());
+        strings.removeAll(strings);
+        strings.addAll(result);
+    }
 
-      return blank.stream()
-              .filter(i -> i.charAt(0) == letter)
-              .sorted()
-              .collect(Collectors.toList());
-  }
+    public static List<Double> getSingularQuadratures(List<Double> numbers) {
+        if (numbers == null || numbers.isEmpty())
+            return null;
+        return numbers.stream()
+                .distinct()
+                .map(i -> i * i)
+                .collect(Collectors.toList());
+    }
 
-  public static <T> T getLastElementOrElseThrow(Collection<T> collection) throws NoSuchElementException{
-      if (collection.isEmpty())
-          throw new NoSuchElementException("No elements in collection");
+    public static List<String> getListStartingWithLetterAndSort(Character letter, Collection<String> blank) {
+        if (!(letter >= 'a' && letter <='z')) {
+            if (!(letter >= 'A' && letter <= 'Z'))
+                return null;
+        }
 
-      return collection.stream()
-              .reduce((first, second) -> second)
-              .get();
-  }
+        if (blank.isEmpty())
+            return null;
 
-  public static int getCountableSumOrElseNull(int[] numbers){
-      int sum = 0;
-      sum = Arrays.stream(numbers)
-              .filter(i-> i%2 == 0)
-              .sum();
-      return sum;
-  }
+        return blank.stream()
+                .filter(s -> !Objects.equals(s, "") && s.length()>0)
+                .filter(i -> i.charAt(0) == letter)
+                .sorted()
+                .collect(Collectors.toList());
+    }
 
-  public static Map<Character,List<String>> getMapWhereFirstLatterIsKey(List<String> list){
-      return list.stream()
-              .collect(Collectors.groupingBy(s -> s.charAt(0)));
-  }
+    public static <T> T getLastElementOrElseThrow(Collection<T> collection) throws NoSuchElementException {
+        if (collection.isEmpty())
+            throw new NoSuchElementException("No elements in collection");
 
+        return collection.stream()
+                .reduce((first, second) -> second)
+                .get();
+    }
+
+    public static int getCountableSumOrElseNull(int[] numbers) {
+        if (numbers == null || numbers.length == 0)
+            return 0;
+        int sum = 0;
+        sum = Arrays.stream(numbers)
+                .filter(i -> i % 2 == 0)
+                .sum();
+        return sum;
+    }
+
+    public static Map<Character, List<String>> getMapWhereFirstLatterIsKey(List<String> list) {
+        if (list == null || list.isEmpty())
+            return null;
+        Map<Character, List<String>> result = list.stream()
+                .filter(s -> !Objects.equals(s, "") && s.length()>0)
+                .distinct()
+                .collect(Collectors.groupingBy(s -> s.charAt(0)));
+        if (result.isEmpty())
+            return null;
+        return result;
+    }
 }
--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -1,22 +1,23 @@
 package lab5;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Map;
 
 import static lab5.Instruments.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayList<String> strings = new ArrayList<>();
-        strings.add("hello");
-        strings.add("lalaA");
-        strings.add("lal");
-        strings.add("lol");
-
-        System.out.println(getMapWhereFirstLatterIsKey(strings));
+        List<String> stringList3 = new ArrayList<>();
+        stringList3.add("qw");
+        stringList3.add("qqer");
+        stringList3.add("wer");
+        stringList3.add("");
+        stringList3.add("awer");
+        stringList3.add("aav");
+        stringList3.add("aav");
+        Map<Character, List<String>> karta = getMapWhereFirstLatterIsKey(stringList3);
+        System.out.println(karta);
 
     }
 }
