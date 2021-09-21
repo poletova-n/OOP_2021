44c5aadce1d3bf764fff4b126a472423f9a856ea
Евгений Смирнов <jekasjeny1012@gmail.com>
2021-09-21 14:07:23 +0300

    new test for  casting

--- src/test/java/com/lab3/Main.java
+++ src/test/java/com/lab3/Main.java
@@ -1,64 +1,88 @@
 package com.lab3;
 
 import lab3.Queue;
-import lab3.animals.classification.cats.DomesticCat;
-import lab3.exceptions.QueueOverFlow;
-import lab3.exceptions.QueueUnderFlow;
+import lab3.animals.classification.cats.*;
+import lab3.exceptions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.Assertions.*;
 
 public class Main {
 
     @Test
     @DisplayName("Queue Underflow Exception")
     public void QueueUnderflowExceptionTest() {
-        Queue<DomesticCat> queue = new Queue<>(10);
+        Queue<Integer> queue = new Queue<>(10, Integer.class);
 
-        Throwable throwable1 = catchThrowable(()->{
-            queue.pop();
-        });
+        Throwable throwable1 = catchThrowable(queue::pop);
         assertThat(throwable1).isInstanceOf(QueueUnderFlow.class);
         assertThat(throwable1.getMessage()).isNotBlank();
 
         Throwable throwable2 = catchThrowable(()->{
-            queue.add(new DomesticCat("a"));
+            queue.add(5);
             queue.pop();
             queue.pop();
         });
         assertThat(throwable2).isInstanceOf(QueueUnderFlow.class);
         assertThat(throwable2.getMessage()).isNotBlank();
-
     }
 
     @Test
     @DisplayName("Queue OverFlow Exception")
     public void QueueOverFlowExceptionTest() {
-        Queue<DomesticCat> queue = new Queue<>(5);
+        Queue<Integer> queue = new Queue<>(5, Integer.class);
 
         Throwable throwable2 = catchThrowable(()->{
-            queue.add(new DomesticCat("a"));
-            queue.add(new DomesticCat("a"));
-            queue.add(new DomesticCat("a"));
-            queue.add(new DomesticCat("a"));
-            queue.add(new DomesticCat("a"));
-            queue.add(new DomesticCat("a"));
-
+            queue.add(1);
+            queue.add(2);
+            queue.add(3);
+            queue.add(4);
+            queue.add(5);
+            queue.add(6);
         });
         assertThat(throwable2).isInstanceOf(QueueOverFlow.class);
         assertThat(throwable2.getMessage()).isNotBlank();
     }
 
     @Test
+    public void QueueIsEmptyTest() throws QueueOverFlow, QueueUnderFlow {
+        Queue<String> queue = new Queue<>(10, String.class);
+        queue.add("lol");
+        queue.add("mem");
+        queue.add("trash");
+
+        Assertions.assertFalse(queue.isFull());
+
+        queue.pop();
+        queue.pop();
+        queue.add("vk");
+        queue.pop();
+        queue.pop();
+
+        Assertions.assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void QueueIsFullTest() throws QueueOverFlow, QueueUnderFlow {
+        Queue<String> queue = new Queue<>(3, String.class);
+        queue.add("lol");
+        queue.add("mem");
+        queue.add("trash");
+        queue.pop();
+        queue.pop();
+        queue.add("vk");
+        queue.add("inst");
+        Assertions.assertTrue(queue.isFull());
+    }
+
+    @Test
     @DisplayName("Correct Job Of Queue")
     public void CorrectJobOfQueue() throws QueueUnderFlow, QueueOverFlow {
-        Queue<DomesticCat> queue = new Queue<>(10);
+        Queue<Cat> queue = new Queue<>(10, Cat.class);
 
         queue.add(new DomesticCat("a"));
-        queue.add(new DomesticCat("b"));
+        queue.add(new ForestCat("b"));
         queue.add(new DomesticCat("c"));
         queue.add(new DomesticCat("d"));
         queue.pop();
@@ -68,6 +92,19 @@ public class Main {
         queue.add(new DomesticCat("f"));
 
         Assertions.assertEquals("Elements=[DomesticCat d, DomesticCat e, DomesticCat f]", queue.toString());
+    }
+
+    @Test
+    @DisplayName("Illegal class Test")
+    public void ClassCastExceptionTest() throws QueueOverFlow {
+        Queue<Integer> queue = new Queue<>(10, Integer.class);
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        Throwable throwable = catchThrowable(()-> queue.add(1.0d));
+
+        assertThat(throwable).isInstanceOf(ClassCastException.class);
+        assertThat(throwable.getMessage()).isNotBlank();
     }
 }
 
