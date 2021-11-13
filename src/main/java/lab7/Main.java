5d2ba7a5c2db3000962056895e589489f4d1bb36
Скорина Кирилл <kirilskorina@gmail.com>
2021-11-13 14:10:05 +0300

    lab7

--- src/main/java/lab7/Main.java
+++ src/main/java/lab7/Main.java
@@ -1,6 +1,35 @@
 package lab7;
 
+import java.util.concurrent.*;
+
 public class Main {
     public static void main(String[] args) {
+        int N=Integer.parseInt(args[0]);
+
+        BlockingQueue<String> blockingQueue = new LinkedBlockingDeque<>();
+        ExecutorService service = Executors.newFixedThreadPool(N);
+
+        for(int i=0;i<N;i++){
+            int num = i;
+            service.execute(()->{
+                try {
+                    String str = Integer.toString(num);
+                    blockingQueue.put("number"+ str);
+                    //Utils.pause(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            service.execute(()->{
+                try {
+                    Utils.pause(3000);
+                    System.out.println(Thread.currentThread()+" read "+blockingQueue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        service.shutdown();
     }
 }
