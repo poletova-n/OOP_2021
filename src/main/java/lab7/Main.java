4b0d3e74dbcabce77f40391afb382065d9d9e7a1
Dmitriy Stakheev <dmitrii.stakhieiev@mail.ru>
2021-12-01 18:51:21 +0300

    lab7 (1.0)

--- src/main/java/lab7/Main.java
+++ src/main/java/lab7/Main.java
@@ -1,6 +1,53 @@
 package lab7;
 
-public class Main {
-    public static void main(String[] args) {
-    }
+import java.util.concurrent.*;
+
+public class Main 
+{
+	public static void main (String[] args)
+	{
+		int iter;
+
+		if (args.length == 0) 
+		{
+			iter = 8;
+		}
+		else 
+		{
+			iter = Integer.parseInt(args[0]);
+		}
+		
+		BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+		ExecutorService service = Executors.newFixedThreadPool(iter);
+		
+		for (int i = 0; i < iter; i++)
+		{
+			int tempI = i;
+
+			service.execute(() -> {
+				try 
+				{
+					queue.put("Message " + Integer.toString(tempI));
+				} 
+				catch (InterruptedException e) 
+				{
+					e.printStackTrace();
+				}
+			});
+
+			service.execute(() -> {
+				try 
+				{
+					Thread.sleep(2000);
+					System.out.println("Thread " + Thread.currentThread().getName() + " read: " + queue.take());
+				} 
+				catch (InterruptedException e) 
+				{
+					e.printStackTrace();
+				}
+			});
+		}
+		
+		service.shutdown();
+	}
 }
