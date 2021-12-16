351c48c2eea8acb3b2762efd4a67ff11f7f8e4d6
Usman Mahmuthazhiev <uzmel89@gmail.com>
2021-12-16 12:36:19 +0300

    lab7

--- src/main/java/lab7/Main.java
+++ src/main/java/lab7/Main.java
@@ -1,6 +1,32 @@
 package lab7;
 
+import java.util.concurrent.*;
+
 public class Main {
     public static void main(String[] args) {
+        int N = 3;
+        if (args.length != 0) {
+            N = Integer.parseInt(args[0]);
+        }
+        BlockingQueue<String> queue = new SynchronousQueue<>();
+        ExecutorService service = Executors.newCachedThreadPool();
+        for (int i = 0; i < N; i ++) {
+            int num = i;
+            service.execute(() -> { // запись потоков
+                try {
+                    queue.put("№ " + Integer.toString(num));;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            service.execute(() -> { // чтение потоков
+                try {
+                    System.out.println(Thread.currentThread() + " read " + queue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        service.shutdown();
     }
 }
