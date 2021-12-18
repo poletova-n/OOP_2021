ffb945d3823d071e2cd81970d16dafa296c2290e
Ade1aida <90214050+Ade1aida@users.noreply.github.com>
2021-12-18 10:13:37 +0300

    small changes 6 and 7

--- src/main/java/lab6/Main.java
+++ src/main/java/lab6/Main.java
@@ -9,15 +9,6 @@ public class Main {
         supervisor.run();
     }
 }
-/*
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Application app = Application.createAndStart();
-        Supervisor supervisor = Supervisor.createAndStart(app);
-    }
-}
- */
+
 
 
