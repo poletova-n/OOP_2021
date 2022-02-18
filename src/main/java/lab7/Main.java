e6bf38941efa49430515e53e1cee161211db97f4
BuildTools <unconfigured@null.spigotmc.org>
2022-02-18 18:24:10 +0300

    Lab7

--- src/main/java/lab7/Main.java
+++ src/main/java/lab7/Main.java
@@ -1,6 +1,32 @@
 package lab7;
 
+import java.util.concurrent.*;
+
 public class Main {
     public static void main(String[] args) {
+        int N = 2;
+        if (args.length != 0) {
+            N = Integer.parseInt(args[0]);
+        }
+        BlockingQueue<String> queue = new SynchronousQueue<>();
+        ExecutorService service = Executors.newCachedThreadPool();
+        for (int i = 0; i < N; i ++) {
+            int num = i;
+            service.execute(() -> {
+                try {
+                    queue.put("№ " + num);;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            service.execute(() -> {
+                try {
+                    System.out.println(Thread.currentThread() + " read " + queue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        service.shutdown();
     }
 }
